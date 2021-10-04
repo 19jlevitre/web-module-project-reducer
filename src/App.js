c42a34c327dbcbb0800d5e0bf77a4e7fbc2db03a
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-10-04 14:04:52 -0600

    some functions and buttons done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 import './App.css';
-
+import reducer, {initialState} from '../src/reducers/index';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
-
+import { addOne } from './actions';
+import { applyNumber } from './actions';
+import { changeOpp } from './actions';
 function App() {
+const [state, dispatch] = useReducer(reducer, initialState);
+//  const handleAddOne = () => {
+//    dispatch(addOne(1));
+//  }
 
-  return (
+const handleApplyNumber = (num) => {
+  dispatch(applyNumber(num));
+}
+const handleChangeOpp = (opp) => {
+  dispatch(changeOpp(opp));
+}
+
+return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
         <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
@@ -17,10 +30,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -30,27 +43,27 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={()=>{handleApplyNumber(1)}}/>
+              <CalcButton value={2} onClick={()=>{handleApplyNumber(2)}}/>
+              <CalcButton value={3} onClick={()=>{handleApplyNumber(3)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={()=>{handleApplyNumber(4)}}/>
+              <CalcButton value={5} onClick={()=>{handleApplyNumber(5)}}/>
+              <CalcButton value={6} onClick={()=>{handleApplyNumber(6)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={()=>{handleApplyNumber(7)}}/>
+              <CalcButton value={8} onClick={()=>{handleApplyNumber(8)}}/>
+              <CalcButton value={9} onClick={()=>{handleApplyNumber(9)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={()=> {handleChangeOpp("+")}}/>
+              <CalcButton value={"*"} onClick={()=> {handleChangeOpp("*")}}/>
+              <CalcButton value={"-"} onClick={()=> {handleChangeOpp("-")}}/>
             </div>
 
             <div className="row ce_button">
